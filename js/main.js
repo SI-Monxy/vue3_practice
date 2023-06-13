386bea86ce745245e63d4b7c35c5d74aeb67b49d
monxy747 <monxy.official@gmail.com>
2023-06-13 23:00:58 +0900

    add 算出プロパティの基本

--- js/main.js
+++ js/main.js
@@ -1,34 +1,11 @@
 const app = Vue.createApp({
     data: () => ({
-        url:'https://www.google.co.jp/'
-        // message:'Hello! Vue.js',
-        // number:100,
-        // ok:true
-        // newItem: '',
-        // todos:[]
-        // message: 'Hello, <span style="color:red;">Vue.js</span>'
+        message: 'Hello!Vue.js'
     }),
-    // methods:{
-    //     clickHandler: function(event){
-    //         this.message = this.message.split('').reverse().join('')
-    //     }
-    // }
-    // methods:{
-    //     addItem: function(event){
-    //         // console.log('Clicked!')
-    //         if(this.newItem === "") return
-    //         let todo = {
-    //             item: this.newItem,
-    //             isDone: false
-    //         }
-    //         this.todos.push(todo)
-    //         this.newItem = ""
-    //     },
-    //     deleteItem: function(index){
-    //         console.log('Delete!')
-    //         // console.log(index)
-    //         this.todos.splice(index,1)
-    //     }
-    // }
+    computed: {
+        reversedMessage: function() {
+            return this.message.split('').reverse().join('')
+        }
+    }
 })
 app.mount('#app')
