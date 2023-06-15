973a196ed9755e10c3700b6e672ab07c4c735bfd
monxy747 <monxy.official@gmail.com>
2023-06-15 23:13:58 +0900

    add 算出プロパティのキャッシュ動作を確認

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 const app = Vue.createApp({
     data: () => ({
-        basePrice: 100
+        
     }),
     computed: {
-        taxIncludedPrice: {
-            get: function() {
-                return this.basePrice * 1.1
-            },
-            set: function(value){
-                this.basePrice = value / 1.1
-
-            }
+        computedNumber: function(){
+            return Math.random()
+        }
+    },
+    methods: {
+        methodNumber: function(){
+            return Math.random()
         }
     }
 })
