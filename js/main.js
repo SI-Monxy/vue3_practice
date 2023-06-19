b20271758164b51b7d2fe0c592604ceb4bd261a2
monxy747 <monxy.official@gmail.com>
2023-06-19 22:41:34 +0900

    add 配列構文でデータバインディング

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
 const app = Vue.createApp({
     data: () => ({
-        isLarge:true,
-        hasError:true
+        largeClass:"large",
+        dangerClass:"text-danger"
     })
 })
 app.mount('#app')
