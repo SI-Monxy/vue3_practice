814144c1abefe204897f5fc69dbb77c965ea1f3e
monxy747 <monxy.official@gmail.com>
2023-06-22 22:55:31 +0900

    add メソッドイベントハンドラ

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@
 const app = Vue.createApp({
     data: () => ({
        counter:0
-    })
+    }),
+    methods:{
+        clickHandler:function(){
+            this.counter++
+        }
+    }
 })
 app.mount('#app')
 
